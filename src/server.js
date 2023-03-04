6e7125650b26a56b306d32b16717fc548999cbe9
Van Quoc <quocx2003@gmail.com>
2023-03-04 10:02:37 +0700

    video7 static file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 
-import express from 'express'
-import configViewEngine from './configs/viewEngine'
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+require('dotenv').config();
 
-const app = express()
-const port = 3005
+const app = express();
+const port = process.env.PORT || 3000;
+
 
 configViewEngine(app);
 
