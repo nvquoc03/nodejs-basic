0bb771b2d9a3725c4e247b73b31489311a81cfe6
Van Quoc <quocx2003@gmail.com>
2023-03-12 10:46:10 +0700

    video16 middleware

--- src/server.js
+++ src/server.js
@@ -6,9 +6,21 @@ import initAPIRoute from './route/api';
 // import connection from './configs/connectDB';
 
 require('dotenv').config();
+var morgan = require('morgan');
+
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use((req, res, next) => { //Khi request hợp lệ thì chạy hàm next
+    console.log('>>>Run into my middleware');
+    console.log(req.method);
+
+    next();
+
+})
+
+app.use(morgan('combined'));
 
 app.use(express.urlencoded({ extended: true }));//-> Giúp gửi data từ Client lên Server và cs thể lấy data 1 cách đơn giản
 app.use(express.json()); //-> Giúp gửi data từ Client lên Server và cs thể lấy data 1 cách đơn giản
@@ -21,6 +33,11 @@ initWebRoute(app);
 
 //init api route
 initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 
 app.listen(port, () => {
